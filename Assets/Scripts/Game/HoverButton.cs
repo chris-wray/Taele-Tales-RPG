4e1ee40c798ccdb80a0c4fa959f1712b9389ca56
Luis Pantin <fefi2000@tamu.edu>
2019-11-17 14:08:53 -0800

    Added .gitignore. Implemented 8-way hover arrow controls

---
+++ Assets/Scripts/Game/HoverButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class HoverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public PlayerControl playerControl;
+    public float axisVertical; // How much it changes the vertical axis movement on the player
+    public float axisHorizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        playerControl.buttonVertical = axisVertical;
+        playerControl.buttonHorizontal = axisHorizontal;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        playerControl.buttonVertical = 0f;
+        playerControl.buttonHorizontal = 0f;
+    }
+}
--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -19,11 +19,17 @@ public class PlayerControl : MonoBehaviour {
     public float attackTime;
     private float attackTimeCounter;
 
+    // Add support for mouse-over controls
+    public float buttonHorizontal;
+    public float buttonVertical;
+
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
 
+        buttonHorizontal = 0f;
+        buttonVertical = 0f;
         if (!playerExists){
             playerExists = true;
             DontDestroyOnLoad(transform.gameObject);
@@ -36,29 +42,38 @@ public class PlayerControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         playerMoving = false;
+        float axisHorizontal = Input.GetAxisRaw("Horizontal");
+        if (buttonHorizontal != 0) {
+            axisHorizontal = buttonHorizontal;
+        }
+
+        float axisVertical = Input.GetAxisRaw("Vertical");
+        if (buttonVertical != 0) {
+            axisVertical = buttonVertical;
+        }
 
         if (!attacking){
-            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            if (axisHorizontal > 0.5f || axisHorizontal < -0.5f)
             {
-                //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-                myRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidBody.velocity.y);
+                //transform.Translate(new Vector3(axisHorizontal * moveSpeed * Time.deltaTime, 0f, 0f));
+                myRigidBody.velocity = new Vector2(axisHorizontal * moveSpeed, myRigidBody.velocity.y);
                 playerMoving = true;
-                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+                lastMove = new Vector2(axisHorizontal, 0f);
             }
 
-            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            if (axisVertical > 0.5f || axisVertical < -0.5f)
             {
-                //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+                //transform.Translate(new Vector3(0f, axisVertical * moveSpeed * Time.deltaTime, 0f));
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, axisVertical * moveSpeed);
                 playerMoving = true;
-                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+                lastMove = new Vector2(0f, axisVertical);
             }
 
-            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            if (axisHorizontal < 0.5f && axisHorizontal > -0.5f)
             {
                 myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y);
             }
-            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -.5f)
+            if (axisVertical < 0.5f && axisVertical > -.5f)
             {
                 myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0f);
             }
@@ -79,8 +94,8 @@ public class PlayerControl : MonoBehaviour {
             anim.SetBool("Attacking", false);
         }
 
-        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetFloat("MoveX", axisHorizontal);
+        anim.SetFloat("MoveY", axisVertical);
         anim.SetFloat("LastMoveY", lastMove.y);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetBool("PlayerMoving", playerMoving);
