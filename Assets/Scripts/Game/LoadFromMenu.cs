8b69d612be148c6583252b87b31741823625efd0
Chris Wray <chriswray@tamu.edu>
2019-11-20 15:38:18 -0600

    BattleUI for Boss + Main Menu

---
+++ Assets/Scripts/Game/LoadFromMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Used to load a new area. A portal.
+public class LoadFromMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnClick()
+    {
+        // Detects a player collision with this portal.
+        Debug.Log("Loading from Menu to Main");
+            Application.LoadLevel("main");
+     
+    }
+}
